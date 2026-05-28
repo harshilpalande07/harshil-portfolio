ff311b73fb8e6c4589b46dfe3df2534da6b83495
harshilpalande07 <harshilpalande@gmail.com>
2026-05-28 10:41:59 +0530

    Reduced bulb glow

--- components/Bulb.jsx
+++ components/Bulb.jsx
@@ -2,15 +2,32 @@ import Image from "next/image";
 
 const Bulb = () => {
   return (
-    <div className="absolute -left-24 bottom-0 rotate-6 opacity-80 z-10 w-[260px] xl:w-[340px] select-none pointer-events-none animate-pulse">
-      <div className="absolute inset-0 bg-cyan-500 blur-[120px] opacity-30 rounded-full"></div>
+    <div
+      className="
+        absolute
+        -left-16
+        bottom-0
+        rotate-6
+        opacity-40
+        xl:opacity-60
+        z-0
+        w-[180px]
+        md:w-[240px]
+        xl:w-[300px]
+        select-none
+        pointer-events-none
+      "
+    >
+      {/* soft glow */}
+      <div className="absolute inset-0 bg-cyan-500 blur-[90px] opacity-10 rounded-full"></div>
 
+      {/* bulb image */}
       <Image
         src="/bulb.png"
         alt="bulb"
-        width={340}
-        height={300}
-        className="w-full h-full drop-shadow-[0_0_35px_#00ffff]"
+        width={300}
+        height={260}
+        className="w-full h-full drop-shadow-[0_0_12px_#00ffff]"
       />
     </div>
   );
